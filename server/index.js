50a74b2a0220f10890e553b7f3540fa01a873445
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-06 01:53:57 -0400

    Create connection to thr db and running the server on port 5000

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
-import cors from "cors" 
+import cors from "cors"
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+const CONNECTIONG_ULR = 'mongodb+srv://MoeMahdi:Moe123@cluster0.qzenxij.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTIONG_ULR, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message))
+
+
