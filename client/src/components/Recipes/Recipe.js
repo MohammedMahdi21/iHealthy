4e8545076ca08c992eb344d5fa5289e681522195
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-09 20:49:53 -0400

    Create Recipes component

---
+++ client/src/components/Recipes/Recipe.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Recipe from "./Recipe/Recipe";
+
+const Recipes = function () {
+  return(
+    <>
+      <h1>
+      Recipes
+      </h1>
+
+      <Recipe />
+      <Recipe />
+    </>
+
+  );
+}
+
+export default Recipes;
