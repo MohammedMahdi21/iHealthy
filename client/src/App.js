dd7ff0fe8b55fdd134cabdf7c3f88830b96b1e73
Mohammed Mahdi <mmd.mahdi21@gmail.com>
2022-09-13 02:24:26 -0400

    use material-ui components to create App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 
-const App = function() {
+import Posts from "./components/Recipes/Recipe";
+import Form from "./components/Form/Form";
+import recipes from './images/recipes.png'
+import useStyles from './styles'
+
+const App = function () {
+  const classes = useStyles();
   return (
-    <div>
-        <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h4" align="center">Recipes</Typography>
+        <img className={classes.image} src={recipes} alt="recipes" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+
   )
 }
 
